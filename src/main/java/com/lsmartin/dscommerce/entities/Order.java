28758ed8ec0bfcd15e92d90700580ad71c42ff43
Leandro Martins <leandro2828@gmail.com>
2024-11-17 22:36:44 -0300

    Criado Payment e configurado seus relacionamentos

--- src/main/java/com/lsmartin/dscommerce/entities/Order.java
+++ src/main/java/com/lsmartin/dscommerce/entities/Order.java
@@ -2,6 +2,7 @@ package com.lsmartin.dscommerce.entities;
 
 import java.time.Instant;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,6 +10,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -26,16 +28,24 @@ public class Order {
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
+	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
 
 	public Order() {
 	}
 
-	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
+	
+
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client, Payment payment) {
 		this.id = id;
 		this.moment = moment;
 		this.orderStatus = orderStatus;
 		this.client = client;
+		this.payment = payment;
 	}
+
+
 
 	public Long getId() {
 		return id;
@@ -67,6 +77,18 @@ public class Order {
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+
+
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
 	}
 	
 	
